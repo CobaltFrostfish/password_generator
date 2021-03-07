605d1f8748359764c23f21624a69721d7996127b
Chris Whalen <christopher.whalen@du.edu>
2021-03-07 15:05:07 -0700

    Major functions and structure of the generator within the JS file are complete

--- Assets/script.js
+++ Assets/script.js
@@ -4,9 +4,8 @@ var generateBtn = document.querySelector("#generate");
 //Variables to be used
 var specialChar = '!@#$%&(*)';
 var numChar = '1234567890';
-var upperChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-var lowerChar = 'abcdefghijklmnopqrstuvwxyz'
-var password = ""
+var upperChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lowerChar = 'abcdefghijklmnopqrstuvwxyz';
 
 function getPasswordOptions() {
   var length = prompt("Choose a password length between 8 and 128 characters.")
@@ -28,6 +27,29 @@ function generatePassword() {
   //This function is going to take in users input and build a password and return password to the right password function
   var options = getPasswordOptions()
   console.log(options)
+
+  var possibleCharacters = ""
+    if (options.numbers) {
+      possibleCharacters += numChar
+      console.log(possibleCharacters)
+    }
+    if (options.uppercase) {
+      possibleCharacters += upperChar
+      console.log(possibleCharacters)
+    }
+    if (options.lowercase) {
+      possibleCharacters += lowerChar
+      console.log(possibleCharacters)
+    }
+    if (options.specialCharacters) {
+      possibleCharacters += specialChar
+      console.log(possibleCharacters)
+    }
+    var results = ""
+    for ( var i = 0; i < options.length; i++ ) {
+      results += possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
+   }
+   return results
 }
 
 
